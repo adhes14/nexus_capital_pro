f7d0941c99b34f0b7ed26f49e3c957e7f4e38066
adhes14 <adhemarduran@gmail.com>
2024-07-21 17:53:15 -0400

    formularios

--- fondos/urls.py
+++ fondos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('contact/<str:name>', views.contact, name='contact'),
+    path('', views.index, name='fondos'),
+    path('fondo_liquidez', views.fondoLiquidezFormView, name='fondo_liquidez'),
 ]
--- fondos/views.py
+++ fondos/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Fondo, FondoLiquidez
+from .form import FondoLiquidezForm
 
 def index(request):
-    return HttpResponse("Hola estamos en el index de la App 'FONDOS'")
+    post_nombre = request.POST.get("nombre")
+    post_fecha_inicio = request.POST.get("fecha_inicio")
+    if post_nombre and post_fecha_inicio:
+        q = Fondo(nombre=post_nombre, fecha_inicio=post_fecha_inicio)
+        q.save()
+
+    filtro_nombre = request.GET.get("nombre")
+    if filtro_nombre:
+        categorias = Fondo.objects.filter(nombre__icontains=filtro_nombre)
+    else:
+        categorias = Fondo.objects.all()
+    return render(request, "fondos/fondos.html", {"categorias": categorias})
+
+
+def contact(request, name):
+    return HttpResponse(f"Hola {name}, estamos en la App 'FONDOS'")
+
+def fondoLiquidezFormView(request):
+    form = FondoLiquidezForm()
+    fondo_liquidez = None
+    id_fondo_liquidez = request.GET.get("id")
+    if id_fondo_liquidez:
+        fondo_liquidez = get_object_or_404(FondoLiquidez, id=id_fondo_liquidez)
+        form = FondoLiquidezForm(instance=fondo_liquidez)
+
+    print(fondo_liquidez)
+
+    if request.method == "POST":
+        if fondo_liquidez:
+            form = FondoLiquidezForm(request.POST, instance=fondo_liquidez)
+        else:
+            form = FondoLiquidezForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, "fondos/fondo_liquidez_form.html", {"form": form})
